ddd408f3c4743124cf9de566a42abbc105f4596d
woooyoung <axdsw121@gmail.com>
2024-06-05 10:26:28 +0900

    assertThat 활성화, 테스트 환경 변경

--- src/main/java/com/ki/level0/p120807/Main.java
+++ src/main/java/com/ki/level0/p120807/Main.java
@@ -2,9 +2,9 @@ package com.ki.level0.p120807;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(2, 3)); // -1
-        System.out.println(new Solution().solution(11, 11)); // 1
-        System.out.println(new Solution().solution(7, 99)); // -1
+//        System.out.println(new Solution().solution(2, 3)); // -1
+//        System.out.println(new Solution().solution(11, 11)); // 1
+//        System.out.println(new Solution().solution(7, 99)); // -1
     }
 }
 
---
+++ src/test/java/com/ki/level0/p120807/Tests.java
@@ -0,0 +1,32 @@
+package com.ki.level0.p120807;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+//        System.out.println(new Solution().solution(2, 3)); // -1
+//        System.out.println(new Solution().solution(11, 11)); // 1
+//        System.out.println(new Solution().solution(7, 99)); // -1
+
+public class Tests {
+
+    @Test
+    @DisplayName("2랑 3을 넣으면 -1이 나와야해")
+    void t1() {
+        assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("11, 11 => 1")
+    void t2() {
+        assertThat(new Solution().solution(11, 11)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("7, 99 => -1")
+    void t3() {
+        assertThat(new Solution().solution(7, 99)).isEqualTo(-1);
+    }
+}
